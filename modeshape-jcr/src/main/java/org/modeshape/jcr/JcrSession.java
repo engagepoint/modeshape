137c94abcecf30d61a9899d5c4dffe67f9d9b53c
Horia Chiorean <hchiorea@redhat.com>
2013-12-13 15:48:01 +0200

    MODE-2117 Updated session.hasPermission so that when readonly-connectors are present, the method returns false for anything else other than the READ and INDEX_WORKSPACE actions.

--- modeshape-jcr/src/main/java/org/modeshape/jcr/JcrSession.java
+++ modeshape-jcr/src/main/java/org/modeshape/jcr/JcrSession.java
@@ -35,6 +35,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
