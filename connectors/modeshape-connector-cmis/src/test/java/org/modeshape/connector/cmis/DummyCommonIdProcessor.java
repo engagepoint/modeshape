f33aeeb2443b7db1d46435567d848ac812be3080
leonid.marushevskiy <leonid.marushevskiy@engagepoint.com>
2014-06-17 11:49:44 +0300

    EDOC-1394: move pattern for virtual ID check to commonIdProcessor

--- connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/cmis/DummyCommonIdProcessor.java
+++ connectors/modeshape-connector-cmis/src/test/java/org/modeshape/connector/cmis/DummyCommonIdProcessor.java
@@ -1,12 +1,15 @@
 package org.modeshape.connector.cmis;
 
-import org.apache.poi.hssf.record.HyperlinkRecord;
 import org.modeshape.connector.cmis.api.SecondaryIdProcessor;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 
 public class DummyCommonIdProcessor implements SecondaryIdProcessor {
+    
+    private static final Pattern INTERNAL_OBJECT_ID_PATTERN = Pattern.compile(".*_.{4}_.{4}_.{4}_.{12}");
+    
     @Override
     public String preProcessIdValue(String id) {
         try {
@@ -20,5 +23,10 @@ public class DummyCommonIdProcessor implements SecondaryIdProcessor {
     public static void main(String[] args) {
         UUID.fromString("c7ae24c3-0915-499d-8b4d-4160a31cb3fb");
         UUID.fromString("idd_c7ae24c3-0915-499d-8b4d-4160a31cb3fb");
+    }
+
+    @Override
+    public boolean isProcessedId(String id) {
+        return INTERNAL_OBJECT_ID_PATTERN.matcher(id).matches();
     }
 }
